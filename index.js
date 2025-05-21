2055179760b10735e6a0941b18ef75eb9f9dcaa0
Madhur Chaturvedi <madhurchaturvedi2000@gmail.com>
2025-05-21 23:20:14 +0530

    layout hear body footer done

--- index.js
+++ index.js
@@ -1,13 +1,95 @@
-// import React from "react";
 import ReactDOM from "react-dom/client";
-// const heading = React.createElement("h1", { id: "heading", color: "red" }, "Namaste React🚀");
 
-const jsx = (
-    <h1 className="item" >
-        Fuck it Worked 🤘🚀
-    </h1>
-)
+const Header = () => {
+    const Logo = {
+        display: "flex",
+        alignItem: "center",
+        justifyContent: "center",
+        gap: "5px",
+    }
+    return (
+        <header className="header">
+            <div className="header-logo" style={Logo}>
+                <img src="https://cdn.worldvectorlogo.com/logos/swiggy-1.svg"
+                    alt="no-food-image"
+                    width={20}
+                />
+
+            </div>
+            <div className="Nav-Links">
+                <ul>
+                    <li>HOME</li>
+                    <li>About</li>
+                    <li>Follow Us</li>
+                    <li>CART-[0]</li>
+                </ul>
+            </div>
+        </header>
+    )
+}
+
+const RestaurantCard = () => {
+    return (
+        <div className="res-card">
+            <img className="res-img" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_292,h_300/TopPicks2024/42148879A.png" alt="" />
+            <h4>Urban Bites</h4>
+            <p>Shahi paneer</p>
+            <p>&#8377;300</p>
+            <p>Rating - &#9733; 4.5</p>
+            <button className="buy">Buy</button>
+        </div>
+    )
+}
+
+const Footer = () => {
+    const Logo = {
+        display: "flex",
+        alignItem: "center",
+        justifyContent: "center",
+        gap: "5px",
+    }
+    return (
+        <footer className="footer" style={{ width: "100vw", height: "15vh", background: "FFF", color: "#8395a7", border: "1px solid black" }}>
+            <br />
+            © 2025 YourCompanyName. All rights reserved.
+        </footer>
+    )
+}
+
+const Body = () => {
+    return (
+        <div className="body">
+            <div className="search">
+                <input type="text" className="search_input_Box" placeholder="Search Food Here... 😋" />
+                <button className="search-btn">Cook Here</button>
+            </div>
+            <div className="res-container">
+                {/* Restaurant Card */}
+                <RestaurantCard />
+                <RestaurantCard />
+                <RestaurantCard />
+                <RestaurantCard />
+                <RestaurantCard />
+                <RestaurantCard />
+                <RestaurantCard />
+                <RestaurantCard />
+                <RestaurantCard />
+                <RestaurantCard />
+
+            </div>
+        </div>
+    )
+}
+
+const AppLayout = () => {
+    return (
+        <div className="App">
+            <Header />
+            <Body />
+            <Footer />
+        </div>
+    )
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(jsx);
+root.render(<AppLayout />);
