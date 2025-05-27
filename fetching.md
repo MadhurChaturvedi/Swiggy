a1d6788c4782404132ceebbac8bdb86ab05fe7dc
Madhur Chaturvedi <madhurchaturvedi2000@gmail.com>
2025-05-28 00:12:10 +0530

    fetch api bug fixed

---
+++ fetching.md
@@ -0,0 +1,6 @@
+1 when to make api call
+
+loading -> render -> API_call -> render
+it like simmer effect
+
+29:43
--- src/Body.js
+++ src/Body.js
@@ -1,11 +1,35 @@
 import { RestaurantCard } from "./Components/RestaurantCard"
 import { BTN_URL } from "./utils/const.js"
-import resList from "./utils/mockData.js"
+// import resList from "./utils/mockData.js"
 
-import { useState } from "react" //this is called named import
+import { useState, useEffect } from "react" //this is called named import
 
 const Body = () => {
-    const [listRestaurant, setListRestaurant] = useState(resList);
+    const [listRestaurant, setListRestaurant] = useState([]);
+    //   useEffect Hook
+
+    const fetchData = async () => {
+        const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=23.2546547&lng=77.4006229&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+        const json = await data.json();
+        console.log(json);
+        const restaurants = json?.data?.cards?.[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants;
+
+        // if (!restaurants) return (<p>Loading...</p>)
+        setListRestaurant(restaurants)
+        console.log(listRestaurant);
+
+
+    }
+
+    // cors polecy
+
+    // two parameter call back function and second one is dependency array
+    useEffect(() => {
+        // console.log("useEffect Called");
+        fetchData()
+    }, []);
+
+
     return (
         <div className="body">
             <div className="search">
