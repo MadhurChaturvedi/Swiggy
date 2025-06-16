56af633da085a6cf46df320131134cfd4e550c8a
Madhur Chaturvedi <madhurchaturvedi2000@gmail.com>
2025-06-16 20:58:52 +0530

    optimization the code learn custom hook fetchMenu

--- src/Components/Restaurantmenu.js
+++ src/Components/Restaurantmenu.js
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from 'react'
-// import Simmer from './Simmer';
+
 import { CDN_URL } from '../utils/const';
-import { Link, useParams } from "react-router"
+import { Link, useParams } from "react-router";
+import useRestaurantMenu from '../utils/useRestaurantMenu.js';
+
 
 export default function Restaurantmenu() {
 
@@ -15,21 +17,23 @@ export default function Restaurantmenu() {
     }
 
 
-    const [resInfo, setResInfo] = useState(null);
+    // const [resInfo, setResInfo] = useState(null);
     const { ID } = useParams();
 
     // console.log(resID);
 
-    useEffect(() => {
-        fetchMenu();
-    }, []);
+    const resInfo = useRestaurantMenu(ID)
 
-    const fetchMenu = async () => {
-        const data = await fetch(`https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=23.2546547&lng=77.4006229&restaurantId=${ID}`)
-        const resjson = await data.json();
-        console.log(resjson);
-        setResInfo(resjson.data)
-    };
+    // useEffect(() => {
+    //     fetchMenu();
+    // }, []);
+
+    // const fetchMenu = async () => {
+    //     const data = await fetch(`https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=23.2546547&lng=77.4006229&restaurantId=${ID}`)
+    //     const resjson = await data.json();
+    //     console.log(resjson);
+    //     setResInfo(resjson.data)
+    // };
 
     if (resInfo === null) return <SimmerSingplePage />
 
@@ -40,11 +44,11 @@ export default function Restaurantmenu() {
     // const costForTwo = resInfo?.cards[2]?.card?.card?.info.costForTwo;
     // const cloudinaryImageId = resInfo?.cards[2]?.card?.card?.info.cloudinaryImageId;
     // const areaName = resInfo?.cards[2]?.card?.card?.info.areaName;
-    const { name, cuisines, costForTwo, cloudinaryImageId, areaName } = resInfo?.cards[2]?.card?.card?.info
+
 
     // const cards = resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR;
     // console.log(itemCards)
-
+    const { name, cuisines, costForTwo, cloudinaryImageId } = resInfo?.cards[2]?.card?.card?.info
 
 
     return (
@@ -60,9 +64,12 @@ export default function Restaurantmenu() {
                 <h1>{name}</h1>
                 <h3>{cuisines}</h3>
                 <h4>₹ {costForTwo}</h4>
-                <h4 style={{ display: "flex", alignItems: "center" }}><img width={30} src="https://cdn-icons-png.flaticon.com/512/5632/5632658.png" alt="" /> {areaName}</h4>
+                {/* <h4 style={{ display: "flex", alignItems: "center" }}><img width={30} src="https://cdn-icons-png.flaticon.com/512/5632/5632658.png" alt="" /> {areaName}</h4> */}
             </div>
         </div>
     )
 
 }
+
+
+// 25:11
---
+++ src/utils/useRestaurantMenu.js
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+
+export default function useRestaurantMenu(resId) {
+    const [resmenu, setResmenu] = useState(null);
+
+    const fetchMenu = async () => {
+        try {
+            const data = await fetch(`https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=23.2546547&lng=77.4006229&restaurantId=${resId}`);
+            const json = await data.json();
+            setResmenu(json.data);
+            console.log(json.data);
+        } catch (error) {
+            console.error("Failed to fetch menu:", error);
+        }
+    };
+    useEffect(() => {
+
+
+        fetchMenu();
+    }, []);
+
+    return resmenu;
+}
