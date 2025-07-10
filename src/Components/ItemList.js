afc9848ee1b5745d4097835181edf80ebf9ae2c7
Madhur Chaturvedi <madhurchaturvedi2000@gmail.com>
2025-07-11 00:05:05 +0530

    add time

--- src/Components/ItemList.js
+++ src/Components/ItemList.js
@@ -2,11 +2,12 @@ import React from 'react'
 
 export default function ItemList({ item }) {
     return (
-        <div className='bg-red-700 mt-9'>
-            <p>{item.category}</p>
+        <div className='bg-red-700 mt-9 m-auto'>
+            <p> name : {item.category}</p>
             <p>{item.description}</p>
             <p>{item.price}</p>
             <p>{item.name}</p>
         </div>
     )
 }
+// 57:03 ep 12
